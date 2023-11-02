24bd0e4f4744da65d8563efc05389edb54bcdb0f
Priyanshu Kumar Sharma <118095177+PriyanshuKSharma@users.noreply.github.com>
2023-11-02 19:49:48 +0530

    demonstration of break and continue keyword

---
+++ Nov2.py
@@ -0,0 +1,18 @@
+#Demonstration of break keyword
+''''no_of_chocolates=20
+chocolates_req=int(input("How many chocolates you want beta !!!"))
+i=1
+while(i<=chocolates_req):
+    if(i>no_of_chocolates):
+        print("Sorry beta out of stock\nBye")
+        break
+    print("Chocolate:",i)
+    i+=1'''
+
+#Demonstration of continue keyword
+#step parameter
+for i in range(1,21):
+    n=i%2
+    if(n!=0):
+        continue
+    print(i)
