a2b5546ec4d1db204bef8ebdb8742dde320a2199
PriyanshuKSharma <priyanshu17ks@gmail.com>
2025-12-20 07:28:09 +0530

    Revision started!!

---
+++ practice.py
@@ -0,0 +1,38 @@
+name="Tony Stark"
+
+"""print(name.replace("Tony", "Mony"))
+print(name.find("S"))
+print("i" in name)
+
+print(5**2)
+print(5>=2)
+
+print(2 > 3 or 3 < 4)
+print(2 > 3 and 3 < 4)"""
+
+# conditional:->
+"""age=float(input("Enter age: "))
+
+if age >=18:
+  print("You are an adult")
+  print("You can vote!!!!")
+
+elif age < 18 and age > 3:
+  print("You are in school")
+
+else:
+  print("You are a toddler!!")"""
+
+# range() -> (0,1,2,3,4)
+print(range(5))
+#Loops
+"""i=0
+while i in range(5):
+  print(i)
+  i=i+1"""
+
+i=5
+while i >= 0:
+  print(i*"*")
+  i=i-1
+
